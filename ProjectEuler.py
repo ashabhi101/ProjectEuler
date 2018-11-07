2741fc0266f74d32fd7ea6342994044dc6d99cf7
Abhilash Reddy Bokka <36679049+ashabhi101@users.noreply.github.com>
2018-11-06 22:07:21 -0700

    Update ProjectEuler.py

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -46,6 +46,81 @@ def primefactors(n):
     print(flist)
     
 primefactors(600851475143)         
+
+  # Question 4
+# Palindrome Number
+  
+def get_digit(number,n):
+    return number//10**n%10
+plist = []
+for i in range(999,100,-1):
+    for j in range(999,100,-1):
+        if len(str(i*j)) ==6:
+            if get_digit(i*j,0) == get_digit(i*j,5) and get_digit(i*j,1) == get_digit(i*j,4) and get_digit(i*j,2) == get_digit(i*j,3):
+                plist.append(i*j)
+    print((sorted(plist)))
+
+ # Question 5
+# Smallest multiple
+from functools import reduce
+from fractions import gcd
+def lcm(a,b):
+    return a*b//gcd(a,b)
+def lcms(*numbers):
+    return reduce(lcm,numbers)
+
+def smallestmultiple(n):
+    smallnumber = 1
+    for i in range(1,n+1):
+        smallnumber = lcms(smallnumber,i)
+    return smallnumber
+        
+smallestmultiple(20) 
+
+# Question 6
+# Square sum difference
+
+def squaresum(n):
+    sumsquare = 0
+    squaresum = 0
+    for i in range(1,n+1):
+        sumsquare = sumsquare+i**2
+        squaresum = (squaresum+i)
+    return (squaresum**2-sumsquare)
+ 
+squaresum(100)
+
+# Question 6
+# Prime NUmber
+
+def primenumber(n):
+    primes = [2]
+    for i in range(3, 10000000000000,2):
+        for j in range(3, math.sqrt(i)):
+            if i%j==0
+
+# Question 7
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
+
+# Question 8
+Largest product in a series
+import re
+number = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
+numlist = str_extract
+
+
        
        
     
